d79d1732dc60a54a6a0bfeba4f566132e93d35db
Turqay Umudzade <bmlk3644j@gmail.com>
2025-11-17 17:14:30 -0500

    Refactor addTwoNumbers function for clarity

--- 2.js
+++ 2.js
@@ -1,39 +1,38 @@
-var addTwoNumbers = function (l1, l2) {
-  let ans = new ListNode()
-  let p = ans
-  let carry = 0
-  let sum = 0
-  while (l1 || l2) {
-    sum = 0
-    if (l1) {
-      sum += l1.val
-      l1 = l1.next
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let out = new ListNode(0)
+    const head = out
+    let carry = 0
+
+    while (l1 || l2) {
+        const l1Val = l1 ? l1.val : 0
+        const l2Val = l2 ? l2.val : 0
+        const sum = l1Val +l2Val + carry
+        if (sum >= 10) carry = 1
+        else carry = 0 
+        out.next = new ListNode(sum%10)
+
+        out = out.next
+        l1 = l1?.next
+        l2 = l2?.next
     }
 
-    if (l2) {
-      sum += l2.val
-      l2 = l2.next
-    }
-
-    if (carry === 1) {
-      sum += carry
-      carry = 0
-    }
-    if (sum > 9) {
-      sum = sum % 10
-      carry = 1
+    if(carry){
+        out.next = new ListNode(1)
     }
 
 
-    p.next = new ListNode(sum)
-    p = p.next
-  }
-
-  if (carry === 1) {
-    p.next = new ListNode(1)
-
-  }
-
-  return ans.next
+    return head.next
+    
 };
-
