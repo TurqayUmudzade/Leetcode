6503558946809adfbadedf1dda3b3c7e11480275
Turqay Umudzade <bmlk3644j@gmail.com>
2025-12-20 19:19:56 -0500

    topological sort
    Implement depth-first search for course scheduling.

--- 0210.js
+++ 0210.js
@@ -1,5 +1,33 @@
-var findOrder = function (numCourses, prerequisites) {
+var findOrder = function(numCourses, prerequisites) {
+    const graph = Array.from({ length: numCourses }, () => []);
 
+    for (const [a, b] of prerequisites) {
+        graph[b].push(a);
+    }
+
+    const out = []
+    const done = new Set()
+    const seen = new Set()
+    const dfs = (node) => {
+        if(done.has(node)) return true
+        if(seen.has(node)) return false
+        
+        seen.add(node)
+        
+        for(const neighbor of graph[node]){
+            if(!dfs(neighbor)) return false
+        }
+
+        seen.delete(node)
+        done.add(node)
+        out.push(node)
+        return true
+    }
+    
+    for(let i = 0; i < numCourses; i++){
+        if(!dfs(i)) return []
+    }
+
+    return out.reverse()
+    
 };
-
-///amazon
