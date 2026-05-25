2594d839ab37abcc33f3c5d25452f87acf54e253
TurqayUmudzade <bmlk3644j@gmail.com>
2026-05-25 13:34:08 +0100

    up

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverageFrom: ['**/*.ts', '!**/*.test.ts', '!node_modules'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    },
+  },
+}
